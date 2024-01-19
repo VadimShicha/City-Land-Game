50b1814973e900f474dfba3970eec4c86cf0e26e
Vadim Shicha <shichavadim@gmail.com>
2024-01-19 00:06:01 -0800

    Smooth camera movement

--- City Land Game/GameScene.swift
+++ City Land Game/GameScene.swift
@@ -11,38 +11,43 @@ import GameplayKit
 class GameScene: SKScene {
     
     var player: SKSpriteNode!
-
+    var cameraNode = SKCameraNode()
+    
+    var lastTouch: CGPoint? //the point of where the last touch happened. nil if there isn't any touches
+    var currentTouchCount: Int = 0 //the current total number of touches on the screen
+    
     override func didMove(to view: SKView) {
         //create the player node
         player = SKSpriteNode(color: SKColor.red, size: CGSize(width: 100, height: 100));
         player.position = CGPoint(x: view.bounds.width / 2, y: view.bounds.height / 2);
-        //player.isUserInteractionEnabled = false;
         self.addChild(player);
-        
-        self.view?.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(self.handlePanCamera)));
+        self.camera = cameraNode;
+        self.addChild(cameraNode);
     }
     
-    @objc func handlePanCamera(gesture: UIPanGestureRecognizer) {
-        
-        if(gesture.state == .changed) {
-            var translation = gesture.translation(in: self.view);
-            if(translation.x > 100) {
-                translation.x = 100;
-            }
-            if(translation.x < -100) {
-                translation.x = -100;
-            }
-            if(translation.y > 100) {
-                translation.y = 100;
-            }
-            if(translation.y < -100) {
-                translation.y = -100;
-            }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        currentTouchCount += 1
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if(currentTouchCount == 1) {
+            let location = touches[touches.index(touches.startIndex, offsetBy: 0)].location(in: self);
             
-            player.position.x += translation.x * 0.01;
-            player.position.y -= translation.y * 0.01;
+            if(lastTouch == nil) {
+                lastTouch = location;
+                return;
+            }
+
+            let cameraMoveAction = SKAction.move(by: CGVector(dx: -(location.x - lastTouch!.x), dy: -(location.y - lastTouch!.y)), duration: 0.1);
+            cameraNode.run(cameraMoveAction);
+            
+            lastTouch = location;
         }
-        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        lastTouch = nil;
+        currentTouchCount -= 1;
     }
     
     override func update(_ currentTime: TimeInterval) {
