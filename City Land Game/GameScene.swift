51ff5968f08c920465f131c33ec84f0cff3714fd
Vadim Shicha <shichavadim@gmail.com>
2024-01-19 16:22:27 -0800

    Noise map for map generation

--- City Land Game/GameScene.swift
+++ City Land Game/GameScene.swift
@@ -22,30 +22,53 @@ class GameScene: SKScene {
     var lastTouch: CGPoint? //the point of where the last touch happened. nil if there isn't any touches
     
     let minCameraScale: CGFloat = 0.3;
-    let maxCameraScale: CGFloat = 5;
+    let maxCameraScale: CGFloat = 8;
     
+    let mapWidth: Int = 30;
+    let mapHeight: Int = 30;
     
     override func didMove(to view: SKView) {
         //create the player node
-//        player = SKSpriteNode(color: SKColor.red, size: CGSize(width: 100, height: 100));
-//        player.position = CGPoint.zero;
-        var lightTile = true;
+        let noiseMap = createNoiseMap(width: CGFloat(mapWidth), height: CGFloat(mapHeight), persistance: 0.65);
         
-        for x in 0...30 {
-            for y in 0...30 {
-                var tileColor = lightTile ? SKColor.green : SKColor.gray;
+        for x in 0..<mapWidth {
+            for y in 0..<mapHeight {
+                let noisePosition = vector2(Int32(x), Int32(y));
+                let noiseValue = noiseMap.value(at: noisePosition);
                 
-                if(x == 0 || x == 30 || y == 0 || y == 30) {
+                var tileColor = SKColor.brown;
+                
+                print(noiseValue);
+                if(noiseValue > 0.85) {
+                    tileColor = SKColor.blue;
+                }
+                else if(noiseValue > 0.75) {
+                    tileColor = SKColor.yellow;
+                }
+                else if(noiseValue > 0.05) {
+                    tileColor = SKColor.green;
+                }
+                else if(noiseValue > -0.3) {
+                    tileColor = SKColor.gray;
+                }
+                else if(noiseValue > -0.7) {
+                    tileColor = SKColor.white;
+                }
+                else {
+                    tileColor = SKColor.cyan;
+                }
+                
+                
+                
+                if(x == 0 || x == mapWidth - 1 || y == 0 || y == mapHeight - 1) {
                     tileColor = SKColor.brown;
                 }
                 
                 let landNode = SKSpriteNode(color: tileColor, size: CGSize(width: 100, height: 100));
                 landNode.position = CGPoint(x: x * 100, y: y * 100);
                 self.addChild(landNode);
-                lightTile = !lightTile;
             }
         }
-        //self.addChild(player);
         cameraNode.position = CGPoint.zero;
         self.camera = cameraNode;
         self.addChild(cameraNode);
@@ -53,6 +76,18 @@ class GameScene: SKScene {
         let pinchGesture = UIPinchGestureRecognizer();
         pinchGesture.addTarget(self, action: #selector(pinchGestureAction(_:)));
         view.addGestureRecognizer(pinchGesture);
+    }
+    
+    func createNoiseMap(width: CGFloat, height: CGFloat, persistance: CGFloat = 0.9) -> GKNoiseMap {
+        let source = GKPerlinNoiseSource();
+        source.persistence = persistance; //how likely the noise values are to change. The higher the more often
+        
+        let noise = GKNoise(source);
+        let size = vector2(1.0, 1.0);
+        let origin = vector2(0.0, 0.0);
+        let sampleCount = vector2(Int32(width), Int32(height))
+        
+        return GKNoiseMap(noise, size: size, origin: origin, sampleCount: sampleCount, seamless: true);
     }
     
     var lastCameraScale: CGFloat = 0
